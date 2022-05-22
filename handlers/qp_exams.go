7621f5ea337bf699d0a65f700da5401016a48408
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-05-22 16:31:40 -0500

    get exam schedule

--- handlers/qp_exams.go
+++ handlers/qp_exams.go
@@ -48,3 +48,40 @@ func GetExamsByQPId(ctx context.Context, questionPaperID *string) ([]*model.Exam
 	}
 	return allSections, nil
 }
+
+func GetExamSchedule(ctx context.Context, examID *string) (*model.ExamSchedule, error) {
+	qryStr := fmt.Sprintf(`SELECT * from qbankz.exam_schedule where exam_id = %s  ALLOW FILTERING`, *examID)
+	getBanks := func() (banks []qbankz.ExamSchedule, err error) {
+		q := global.CassSession.Session.Query(qryStr, nil)
+		defer q.Release()
+		iter := q.Iter()
+		return banks, iter.Select(&banks)
+	}
+	banks, err := getBanks()
+	if err != nil {
+		return nil, err
+	}
+	allSections := make([]*model.ExamSchedule, 0)
+	for _, bank := range banks {
+		copiedQuestion := bank
+		createdAt := strconv.FormatInt(copiedQuestion.CreatedAt, 10)
+		updatedAt := strconv.FormatInt(copiedQuestion.UpdatedAt, 10)
+		bufferTimeInt := strconv.Itoa(copiedQuestion.BufferTime)
+		startInt := strconv.FormatInt(copiedQuestion.Start, 10)
+		endInt := strconv.FormatInt(copiedQuestion.End, 10)
+		currentQuestion := &model.ExamSchedule{
+			ID:         &copiedQuestion.ID,
+			CreatedBy:  &copiedQuestion.CreatedBy,
+			CreatedAt:  &createdAt,
+			UpdatedBy:  &copiedQuestion.UpdatedBy,
+			UpdatedAt:  &updatedAt,
+			ExamID:     &copiedQuestion.ExamID,
+			BufferTime: &bufferTimeInt,
+			Start:      &startInt,
+			End:        &endInt,
+			IsActive:   &copiedQuestion.IsActive,
+		}
+		allSections = append(allSections, currentQuestion)
+	}
+	return allSections[0], nil
+}
