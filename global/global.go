49edd7729fc536dfed446534f9b68b2c78b7a268
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-06-03 05:36:11 -0500

    formats

--- global/global.go
+++ global/global.go
@@ -4,16 +4,15 @@ import (
 	"context"
 	"sync"
 
-	"github.com/zicops/zicops-course-query/lib/db/cassandra"
 	cry "github.com/zicops/zicops-course-query/lib/crypto"
-
+	"github.com/zicops/zicops-course-query/lib/db/cassandra"
 )
 
 // some global variables commonly used
 var (
 	CTX             context.Context
 	CassSession     *cassandra.Cassandra
-	CryptSession	*cry.Cryptography
+	CryptSession    *cry.Cryptography
 	Cancel          context.CancelFunc
 	WaitGroupServer sync.WaitGroup
 )
